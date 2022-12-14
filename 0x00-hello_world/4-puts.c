8f10daf1120c3e86dc3101b3b17fd9a9c8faf5a5
root <Chrisjsm>
2022-12-13 19:58:01 -0800

    first c-program with vim

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include(stdio.h>
+/*
+ * main -to print with puts inbuild func
+ * return: always 0
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	}
