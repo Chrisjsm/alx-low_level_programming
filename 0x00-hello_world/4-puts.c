8d1e4c296ef36285fa893eb540c778ea5f97c3c1
root <Chrisjsm>
2022-12-13 20:09:57 -0800

    ty style of first coding c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include(stdio.h>
 /*
  * main -to print with puts inbuild func
+ *
  * return: always 0
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
