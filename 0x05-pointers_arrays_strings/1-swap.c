9e86ad693b679a6e350ee098490e75349fc1bfee
chrisjsm <jameschristo96@gmail.com>
2022-12-19 09:58:07 +0100

    for firt 3

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int -swaps the values of two integer
+ * @a: integer to swap
+ * @b: int to swap
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+	m = *a;
+	*a = *b
+	*b = m;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the len of a str
+ * @s: string
+ * Return: len
+ */
+int swap_int(char *s)
+{
+        int longi = 0;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+
+}
+
