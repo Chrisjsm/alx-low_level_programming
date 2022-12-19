dff4c8b17c63697b75d98043b1ad362afdb15624
chrisjsm <jameschristo96@gmail.com>
2022-12-19 17:44:45 +0100

    the length checker fun

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _strlen - returns the len of a str
+ * _strlen - returns the length of a string
  * @s: string
- * Return: len
+ * Return: length
  */
-int swap_int(char *s)
+int _strlen(char *s)
 {
         int longi = 0;
+
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
 	return (longi);
-
 }
 
