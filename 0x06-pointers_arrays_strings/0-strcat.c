83b74b76e229730db3340780c95ec729699a417e
chrisjsm <jameschristo96@gmail.com>
2022-12-21 23:37:55 +0100

    first commit on this one

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,13 @@
                                                                                                                                      
 /**                                                                                                                                  
  * strcat - Concatenates the string pointed to by @src, including the x                                                     
- *          null byte, to the end of the string pointed to by @dest.                                                                 
+ * null byte, to the end of the string pointed to by @dest.                                                                 
  * @dest: A pointer to the string to be concatenated upon.                                                                           
  * @src: The source string to be appended to @dest.                                                                                  
  *                                                                                                                                   
  * Return: A pointer to the destination string @dest.                                                                                
  */                                                                                                                                  
-char *_strcat(char *dest, const char *src)                                                                                            
+char *strcat(char *dest, const char *src)                                                                                            
 {                                                                                                                                    
         int index = 0, dest_len = 0;                                                                                                 
                                                                                                                                      
