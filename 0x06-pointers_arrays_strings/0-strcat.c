2e358c78753ebe0f90073b4e0f76108f5a5d98ce
chrisjsm <jameschristo96@gmail.com>
2022-12-22 04:36:06 +0100

    just debugged 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
-#include "main.h"                                                                                                                    
-                                                                                                                                     
+#include "main.h"                                                                                                                                    
 /**                                                                                                                                  
  * strcat - Concatenates the string pointed to by @src, including the x                                                     
  * null byte, to the end of the string pointed to by @dest.                                                                 
@@ -8,17 +7,15 @@
  *                                                                                                                                   
  * Return: A pointer to the destination string @dest.                                                                                
  */                                                                                                                                  
-char *strcat(char *dest, const char *src)                                                                                            
+char *strcat(char *dest, const char *src)                                 
 {                                                                                                                                    
-        int index = 0, dest_len = 0;                                                                                                 
+        int index = 0, dest_len = 0;                                                                                            
+                                                                                                                                    
+        while (dest[index++])                                                                                                  
+                dest_len++;                                                                                                        
                                                                                                                                      
-        while (dest[index++])                                                                                                        
-                dest_len++;                                                                                                          
-                                                                                                                                     
-        for (index = 0; src[index]; index++)                                                                                         
-                dest[dest_len++] = src[index];                                                                                       
-                                                                                                                                     
-        return (dest);                                                                                                               
+        for (index = 0; src[index]; index++)                                                                                       
+                dest[dest_len++] = src[index];                                                                            
+                                                                                                                             
+        return (dest);                                                                                               
 }
-
-
