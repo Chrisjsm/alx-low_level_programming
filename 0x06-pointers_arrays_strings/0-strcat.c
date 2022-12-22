cac221e64a699c9136373959554ee42c63f261f0
chrisjsm <jameschristo96@gmail.com>
2022-12-22 06:20:48 +0100

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,44 @@
-#include "main.h"                                                                                               
-/**                                                                                                                    
- * strcat - Concatenates the string pointed to by @src, including the x                                                     
- * null byte, to the end of the string pointed to by @dest.                                                                 
- * @dest: A pointer to the string to be concatenated upon.                                                                           
- * @src: The source string to be appended to @dest.                                                                                  
- *                                                                                                                                   
- * Return: A pointer to the destination string @dest.                                                                                
- */                                                                                                                                  
-char *strcat(char *dest, const char *src)                                 
+/**                                                                             
+ * strcat - Concatenates the string pointed to by @src, including the x         
+ * null byte, to the end of the string pointed to by @dest.                     
+ * @dest: A pointer to the string to be concatenated upon.                      
+ * @src: The source string to be appended to @dest.                             
+ *                                                                              
+ * Return: A pointer to the destination string @dest.                           
+ */                                                                             
+char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;                                                                                                                            
-        while (dest[index++])                                                                                                  
-                dest_len++;                                                                                                 
-        for (index = 0; src[index]; index++)                                                                                     
-                dest[dest_len++] = src[index];                                                                               
-        return (dest);                                                          }
+        int index = 0, dest_len = 0;                                            
+#include "main.h"
+
+/**
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
+ */
+char *_strcat(char *dest, char *src)
+{
+        char *s = dest;
+
+        while (*dest != '\0')
+        {
+                dest++;
+        }
+
+        while (*src != '\0')
+        {
+                *dest = *src;
+                dest++;
+                src++;
+        }
+        *dest = '\0';
+        return (s);
+}                          38,1 Bot
+-- INSERT --
+
+
+
+
+
+
