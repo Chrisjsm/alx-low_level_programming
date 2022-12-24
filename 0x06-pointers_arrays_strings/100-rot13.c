178945c542e5f24bd20f63d8a12ad28518fa1707
root <Chrisjsm>
2022-12-24 02:29:51 -0800

    this

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,19 +1,25 @@
 #include "main.h"
 /**
  * rot13 - Encodes a string using rot13.
+ * @str - param to be encodedded with  rot13
  * Return: A pointer to the encoded string.
  */
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
-	char alphabet[52] = {
-		'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 
-		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
-		'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-	};
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+	       	'G', 'H', 'I', 'J', 'K', 'L', 
+		'M', 'N', 'O', 'P','Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+	       	'e', 'f', 'g', 'h', 'i', 'j',
+	       	'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v',
+	       	'w', 'x', 'y', 'z'};
 	char rot13key[52] = {
-		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 
-		'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+		'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 
+		'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',
+	       	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 
 		'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 	};
 
@@ -32,3 +38,24 @@ char *rot13(char *str)
 
 	return (str);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
