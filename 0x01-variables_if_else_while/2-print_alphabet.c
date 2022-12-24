a9e03e59cac8c39cc289ffc386e3d37e8d5f6091
root <Chrisjsm>
2022-12-24 09:04:05 -0800

    tired joor

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0-alwys
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('n\');
+	return (0);
+}
