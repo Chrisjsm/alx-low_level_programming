39610d9e36fc4be4aea670baecd90334fe4a6595
root <Chrisjsm>
2022-12-24 04:46:32 -0800

    betty wahala too much

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,4 @@
-6-cap_string.c
-
 #include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
@@ -14,7 +11,6 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-<<<<<<< HEAD
 		while (!(str[index] >= 'a' && str[index] <= 'Z'))
 			index++;
 		if (str[index - 1] == '' ||
@@ -32,30 +28,8 @@ char *cap_string(char *str)
 				str[index - 1] == '}' ||
 				index == 0)
 			str[index] -= 32;
-=======
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
->>>>>>> a5197d95f0bf3632c46deff83167de81254ee07d
 		index++;
 	}
 
 	return (str);
 }
-	
