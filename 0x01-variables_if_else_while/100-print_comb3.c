cc936ee2ae01ff9eadd916ac941e9a1352dd84c6
root <Chrisjsm>
2022-12-25 05:22:03 -0800

    kkk

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 /**
+<<<<<<< HEAD
  * main - entry
  *
  * Return: Always 0
+=======
+ * main - ntry
+ *
+ * Return: always 0
+>>>>>>> ae934acd022dd197bc1b36dae92fdc56418db645
  */
 int main(void)
 {
 	int d, p;
 
+<<<<<<< HEAD
 	for (d = '0'; d < '9' d++)
+=======
+	for (d = '0'; d < '9'; d++)
+>>>>>>> ae934acd022dd197bc1b36dae92fdc56418db645
 	{
 		for (p = d + 1; p <= '9'; p++)
 		{
