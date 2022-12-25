2a4fd14fb86db1891a9dafc48eb0d948828e596b
root <Chrisjsm>
2022-12-24 21:58:49 -0800

    print como

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0always
+ */
+int main(void)
+{
+	int  a;
+
+	for (a = 0; a < 100; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
