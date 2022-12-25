42c899b91653c14f9eac1bf9a6a78a435fb8315c
root <Chrisjsm>
2022-12-24 22:17:01 -0800

    print cobo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,13 +2,13 @@
 /**
  * main - entry
  *
- * Return: 0always
+ * Return: 0 always
  */
 int main(void)
 {
 	int  a;
 
-	for (a = 0; a < 100; a++)
+	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
