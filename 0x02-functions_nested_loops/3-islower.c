1019c76d15789ca39198e844ba611f85941ac37c
root <Chrisjsm>
2022-12-25 08:21:19 -0800

    islower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *  _islower - pls checkfor lower case
- *
+ * _islower - pls checkfor lower case
+ * @c: represents lower cased letter
  * Return: 1 if c else 0
  */
 int _islower(int c)
