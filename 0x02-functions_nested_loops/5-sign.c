fa67968d4d56424adb3c9079fea07f7155107031
root <Chrisjsm>
2022-12-25 08:52:38 -0800

    int print_sign(int n)

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,24 @@
 #include "main.h"
 /**
- *
+ * print_sign - print sign of a num
+ * @n: num be checked
+ *Return:1 && print + if n > 0 :-1 && print - if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * int _abs - find abs val of an int
+ *
+ * Return: 0
+ */
+int _abs(int)
+{
+
+}
