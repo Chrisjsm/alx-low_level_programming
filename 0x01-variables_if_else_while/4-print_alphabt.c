edf87f7a05d379733effaea20b660bc7feb10aef
root <Chrisjsm>
2022-12-24 21:14:05 -0800

    still on alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ *
+ * Return:  0 always is a success!
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
