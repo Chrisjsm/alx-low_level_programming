624f72d2f5cf03e63438d7aec21f226ba0836ae3
root <Chrisjsm>
2022-12-25 22:13:09 -0800

    so i fixed conflicing types

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,8 +11,8 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
-void *_strcpy(char *dest, char *src);
 
 #endif
