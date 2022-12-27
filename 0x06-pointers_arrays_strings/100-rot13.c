84470513bd5a1bfc3fd8ae380a034591d8f8277c
root <Chrisjsm>
2022-12-27 06:58:09 -0800

    i just fixed betty error no desc for ..

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,41 +1,28 @@
 #include "main.h"
+
 /**
  * rot13 - Encodes a string using rot13.
- * @str - param to be encodedded with  rot13
- * Return: A pointer to the encoded string.
+ * @str: param to be encoded with  rot13
+ *
+ * Return: address of str
  */
-char *rot13(char *str)
+char *rot13(char str)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-	       	'G', 'H', 'I', 'J', 'K', 'L', 
-		'M', 'N', 'O', 'P','Q', 'R',
-		'S', 'T', 'U', 'V', 'W', 'X',
-		'Y', 'Z', 'a', 'b', 'c', 'd',
-	       	'e', 'f', 'g', 'h', 'i', 'j',
-	       	'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v',
-	       	'w', 'x', 'y', 'z'};
-	char rot13key[52] = {
-		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
-		'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 
-		'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',
-	       	'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 
-		'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
-	};
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (str[indx1])
+	for (i = 0; *(str + i); i++)
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+		for (j = 0; j < 52; j++)
 		{
-			if (str[indx1] == alphabet[indx2])
+			if (a[j] == *(str + i))
 			{
-				str[indx1] = rot13key[indx2];
+				*(str + i) = b[j];
 				break;
 			}
 		}
-		indx1++;
 	}
-
 	return (str);
 }
 
