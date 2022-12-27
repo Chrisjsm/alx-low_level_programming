d7dbb8aa3dbef2b2edbbb71e12390124cc09339b
chrisjsm <jameschristo96@gmail.com>
2022-12-27 17:53:12 +0100

    no 3 pointers task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
- * _puts - print a string and new line to stdout
- * @str: string to be printed
+ * _puts - prints a string
+ * @str: the string
+ *
  * Return: the length of the string
  */
 void _puts(char *str)
@@ -11,6 +13,7 @@ void _puts(char *str)
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
